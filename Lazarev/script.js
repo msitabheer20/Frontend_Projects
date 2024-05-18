26d87f4b08bfe9e7bcdb5e8dda48d730a46b6e7c
msitabheer20 <msitabheerdey@gmail.com>
2024-05-18 23:41:20 +0530

    added nav animations

---
+++ Lazarev/script.js
@@ -0,0 +1,44 @@
+function navAnimation() {
+    var nav = document.querySelector("nav")
+
+    nav.addEventListener("mouseenter", function () {
+        let tl = gsap.timeline()
+        tl.to('#nav-bt', {
+            height: "26vh"
+        })
+
+        tl.to('.nav-2 h5', {
+            display: "block"
+        })
+
+        tl.to(".nav-2 h5 span", {
+            y: 0,
+            stagger: {
+                amount: 0.5
+            }
+        })
+    })
+
+    nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
+
+        tl.to(".nav-2 h5 span", {
+            y: 25,
+            stagger: {
+                amount: 0.2
+            }
+        })
+
+        tl.to('.nav-2 h5', {
+            display: "none",
+            duration: 0.1
+        })
+
+        tl.to("#nav-bt", {
+            height: 0,
+            duration: 0.2
+        })
+    })
+}
+
+navAnimation();
